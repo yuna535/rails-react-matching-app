e06c55bad1fadb3264ca304d952c86ac63331609
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-02-06 14:18:42 +0900

    APIクライアントを作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,24 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { useEffect, useState } from "react"
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React from "react"
+import { execTest } from "lib/api/test"
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState<string>("")
+
+  const handleExecTest = async () => {
+    const res = await execTest()
+
+    if (res.status === 200) {
+      setMessage(res.data.message)
+    }
+  }
+
+  useEffect(() => {
+    handleExecTest()
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <h1>{message}</h1>
   )
 }
 

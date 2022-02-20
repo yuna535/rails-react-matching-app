a0714c265b21e6ff446f9436ea1c9052624ab0b7
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-02-20 15:58:10 +0900

    エラー解消

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -16,9 +16,9 @@ export const signIn = (data: SignInData)  => {
 // サインアウト（ログアウト）
 export const signOut = () => {
   return client.delete("auth/sign_out", { headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+    "access-token": Cookies.get("_access_token") || "",
+    "client": Cookies.get("_client") || "",
+    "uid": Cookies.get("_uid") || "",
   }})  
 }
 
@@ -26,8 +26,8 @@ export const signOut = () => {
 export const getCurrentUser = () => {
   if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
   return client.get("/auth/sessions", { headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+    "access-token": Cookies.get("_access_token") || "",
+    "client": Cookies.get("_client") || "",
+    "uid": Cookies.get("_uid") || "",
   }})
 }
